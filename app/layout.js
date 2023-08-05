91e2b05efa38d5355c1ae209d8c0237c6d94bf44
Kyle Rehak <kylerehak@Kyles-MacBook-Pro-2.local>
2023-08-05 20:59:28 +0800

    Add ChakraUI

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { AuthContextProvider } from "@/app/context/AuthContext";
-
+import { ChakraProvider } from "@chakra-ui/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <AuthContextProvider>{children}</AuthContextProvider>
+        <ChakraProvider>
+          <AuthContextProvider>{children}</AuthContextProvider>
+        </ChakraProvider>
       </body>
     </html>
   );
