500b544f711bc39740508fcf4d360bd8e621cbc5
Kyle Rehak <kylerehak@Kyles-MacBook-Pro-2.local>
2023-08-09 20:19:30 +0800

    Implement menu bar

--- components/MenuBar.jsx
+++ components/MenuBar.jsx
@@ -2,17 +2,22 @@
 import { CalendarIcon, EditIcon, HamburgerIcon } from '@chakra-ui/icons'
 import { Flex, Spacer, Link, Box } from '@chakra-ui/react'
 import NextLink from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 const MenuBar = () => {
+    let path = usePathname();
+
+
   return (
     <Box pos="absolute" top={0} h="100vh" w="100vw" zIndex={-99}>
     <Flex p='6' pos="absolute" bottom={0} w="100%">
-        <Link as={NextLink} href='/workout'><EditIcon boxSize={6}/></Link>
+        <Link as={NextLink} href='/workout'>{path === '/workout' ? <EditIcon boxSize={6} color="red"/> : <EditIcon boxSize={6} />}</Link>
         <Spacer></Spacer>
-        <Link as={NextLink} href='/history'><CalendarIcon boxSize={6}/></Link>
+        <Link as={NextLink} href='/history'>{path === '/history' ? <CalendarIcon boxSize={6} color="red"/> : <CalendarIcon boxSize={6} />}</Link>
         <Spacer></Spacer>
-        <Link as={NextLink} href='/settings'><HamburgerIcon boxSize={8}/></Link>
+        <Link as={NextLink} href='/settings'>{path === '/settings' ? <HamburgerIcon boxSize={8} color="red"/> : <HamburgerIcon boxSize={8} />}</Link>
     </Flex>
     </Box>
   )
