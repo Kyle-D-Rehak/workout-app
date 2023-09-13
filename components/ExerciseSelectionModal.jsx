f3f9f969c08c6fbd6347a8a836589b4651db1c57
Kyle Rehak <kylerehak@Kyles-MacBook-Pro-2.local>
2023-09-13 20:44:08 +0800

    Implement exercise type filter

--- components/ExerciseSelectionModal.jsx
+++ components/ExerciseSelectionModal.jsx
@@ -1,99 +1,188 @@
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    useDisclosure,
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-    Input,
-    InputGroup,
-    InputRightElement,
-  } from '@chakra-ui/react'
-  import exercises from '@/app/exercises'
-import { SearchIcon } from '@chakra-ui/icons'
-
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Select,
+} from "@chakra-ui/react";
+import exercises from "@/app/exercises";
+import { SearchIcon } from "@chakra-ui/icons";
 
 const ExerciseSelectionModal = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const [searchValue, setSearchValue] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [searchValue, setSearchValue] = useState("");
+  const [type, setType] = useState();
 
-    return (
-      <React.Fragment>
-        <Button onClick={onOpen}>Exercises</Button>
-  
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent maxH='80%'>
-            <ModalHeader>
-                <InputGroup >
-                <Input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} mr={6} placeholder='Search...' />
-                <InputRightElement mr={6} pointerEvents='none'>
-                    <SearchIcon />
-                </InputRightElement>
-                </InputGroup>
-            </ModalHeader>
-            <ModalCloseButton />
+  return (
+    <React.Fragment>
+      <Button onClick={onOpen}>Exercises</Button>
 
-            <ModalBody overflowX='hidden' overflowY='scroll'>
-              <TableContainer overflowX='hidden'>
-                <Table>
-                    <Tbody>
-                {exercises.map((section) => {
-                    if(searchValue === "" || searchValue === undefined) {
-                        return(
-                            <>
-                                <Tr key={section.letter}>
-                                    <Th>{section.letter}</Th>
-                                </Tr>
-                                {section.exercises.map((index) => (
-                                    <Tr key={index.exercise}>
-                                        <Td _hover={{cursor: 'pointer', color: 'blue.500'}}>{index.exercise}</Td>
-                                    </Tr>
-                                ))}
-                            </>
-                    )} else return (
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent maxH="80%">
+          <ModalHeader>
+            <InputGroup>
+              <Input
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                mr={6}
+                placeholder="Search..."
+              />
+              <InputRightElement mr={6} pointerEvents="none">
+                <SearchIcon />
+              </InputRightElement>
+            </InputGroup>
+            <Select
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+              placeholder="Exercise type"
+            >
+              <option value="Any">Any</option>
+              <option value="Abductors">Abductors</option>
+              <option value="Abs">Abs</option>
+              <option value="Adductors">Adductors</option>
+              <option value="Biceps">Biceps</option>
+              <option value="Calves">Calves</option>
+              <option value="Cardio">Cardio</option>
+              <option value="Chest">Chest</option>
+              <option value="Core">Core</option>
+              <option value="Forearms">Forearms</option>
+              <option value="Glutes">Glutes</option>
+              <option value="Hamstrings">Hamstrings</option>
+              <option value="Lats Back">Lats Back</option>
+              <option value="Lower Back">Lower Back</option>
+              <option value="Middle Back">Middle Back</option>
+              <option value="Miscellaneous">Miscellaneous</option>
+              <option value="Neck">Neck</option>
+              <option value="Plyometrics">Plyometrics</option>
+              <option value="Quadriceps">Quadriceps</option>
+              <option value="Shoulders">Shoulders</option>
+              <option value="Stretching">Stretching</option>
+              <option value="Traps">Traps</option>
+              <option value="Triceps">Triceps</option>
+            </Select>
+          </ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody overflowX="hidden" overflowY="scroll">
+            <TableContainer overflowX="hidden">
+              <Table>
+                <Tbody>
+                  {exercises.map((section) => {
+                    if (
+                      (searchValue === "" || searchValue === undefined) &&
+                      (type === undefined || type === "Any" || type === "")
+                    ) {
+                      return (
                         <>
+                          <Tr key={section.letter}>
+                            <Th>{section.letter}</Th>
+                          </Tr>
+                          {section.exercises.map((index) => (
+                            <Tr key={index.exercise}>
+                              <Td
+                                _hover={{
+                                  cursor: "pointer",
+                                  color: "blue.500",
+                                }}
+                              >
+                                {index.exercise}
+                              </Td>
+                            </Tr>
+                          ))}
+                        </>
+                      );
+                    } else {
+                      if (
+                        (searchValue === "" || searchValue === undefined) &&
+                        type !== undefined &&
+                        type !== "Any" &&
+                        type !== ""
+                      ) {
+                        return (
+                          <>
                             {section.exercises.map((index) => {
-                                if(index.exercise.toLowerCase().includes(searchValue.toLowerCase()) || searchValue === "") {
-                                    return (
-                                        <Tr key={index.exercise}>
-                                            <Td _hover={{cursor: 'pointer', color: 'blue.500'}}>{index.exercise}</Td>
-                                        </Tr>
-                                    )                           
-                                }
+                              if (type === index.muscle) {
+                                return (
+                                  <Tr key={index.exercise}>
+                                    <Td
+                                      _hover={{
+                                        cursor: "pointer",
+                                        color: "blue.500",
+                                      }}
+                                    >
+                                      {index.exercise}
+                                    </Td>
+                                  </Tr>
+                                );
+                              }
                             })}
-                    </>
-                    )
-                
-})}
-                    </Tbody>
-                </Table>
-              </TableContainer>
-            </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme='blue' w='100%' onClick={onClose}>
-                Add Exercise
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </React.Fragment>
-    )
-}
+                          </>
+                        );
+                      } else
+                        return (
+                          <>
+                            {section.exercises.map((index) => {
+                              if (
+                                index.exercise
+                                  .toLowerCase()
+                                  .includes(searchValue.toLowerCase()) &&
+                                (type === undefined ||
+                                  type === "Any" ||
+                                  type === "" ||
+                                  type === index.muscle)
+                              ) {
+                                return (
+                                  <Tr key={index.exercise}>
+                                    <Td
+                                      _hover={{
+                                        cursor: "pointer",
+                                        color: "blue.500",
+                                      }}
+                                    >
+                                      {index.exercise}
+                                    </Td>
+                                  </Tr>
+                                );
+                              }
+                            })}
+                          </>
+                        );
+                    }
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </ModalBody>
 
-export default ExerciseSelectionModal
+          <ModalFooter>
+            <Button colorScheme="blue" w="100%" onClick={onClose}>
+              Add Exercise
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+export default ExerciseSelectionModal;
